bf2da9c4c098912c81dfd697bee1fe019bc93374
GeorgeDzigo <mr.georgegamertv@gmail.com>
2021-09-06 21:29:02 +0300

    Putted DB configuration in constants

--- db.php
+++ db.php
@@ -1,7 +1,11 @@
 <?php
 class Connection {
+      const HOST = "localhost";
+      const DBNAME = "id17519548_scandiwebtask";
+      const DBTABLE = "id17519548_rooot";
+      const PASSWORD = "2+Vai/U^e&V/[2a2";
       public static function db() {
-      $pdo = new PDO("mysql:host=localhost;dbname=id17519548_scandiwebtask", "id17519548_rooot", "2+Vai/U^e&V/[2a2");
+      $pdo = new PDO("mysql:host=".self::HOST.";dbname=".self::DBNAME, self::DBTABLE, self::PASSWORD);
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $pdo;
       }
